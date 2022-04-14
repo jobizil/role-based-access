545cdffca7189bdb4feb3d0ef8aa11d20aaa3c47
Ugbem Job <ugbemjob57@gmail.com>
2022-04-14 10:56:00 +0100

    Managing users in RBAC

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,12 +1,31 @@
 const { Router } = require('express')
+const mongooose = require('mongoose')
+
 const User = require('../models/user.model')
 const router = Router()
 
 router.get('/users', async (req, res, next) => {
   try {
     const users = await User.find({})
-    res.send(users)
+    res.render('manage-users', { users })
     // res.render('users', { users })
+  } catch (error) {
+    next(error)
+  }
+})
+router.get('/user/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+
+    if (!mongooose.Types.ObjectId.isValid(id)) {
+      req.flash('error', 'Invalid ID')
+      res.redirect('/admin/users')
+      return
+    }
+
+    const person = await User.findById(id)
+
+    res.render('profile', { person })
   } catch (error) {
     next(error)
   }
